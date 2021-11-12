2182bae80fc9a435dd50d32aaaf0476a949e2c55
akagimax <84302951+akagimax@users.noreply.github.com>
2021-11-12 16:25:54 +0900

    Add files via upload

---
+++ unzipper.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d %~dp0
+
+dir /b /s *.zip>%~dp0zips.txt
+
+for /f "delims=" %%a in (%~dp0zips.txt) do (
+powershell -command "Expand-Archive -Path \"%%a\" -DestinationPath \"%%~na\""
+pause
+)
